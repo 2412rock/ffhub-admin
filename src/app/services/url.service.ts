09676c08f94dbef0956e9964014d88649a58a7b1
Adrian Albu <2412rock@gmail.com>
2025-02-24 14:52:33 +0200

    update

--- src/app/services/url.service.ts
+++ src/app/services/url.service.ts
@@ -8,7 +8,7 @@ export class UrlService {
   private devMode = isDevMode();
     //private apiUrl = 'http://192.168.1.134:4200'
     //private sockerService = "ws://192.168.1.134:4200/ws/"
-     private apiUrl = 'http://ffhub.co:4500';
+     private apiUrl = 'https://ffhub.co:4500';
 
   constructor() { }
 
